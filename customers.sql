f4f1c91d544fa6874310ad055d927b2f7dd3974f
iriscano27 <45745483+iriscano27@users.noreply.github.com>
2023-02-25 17:23:54 +0000

    Add customers query

---
+++ customers.sql
@@ -0,0 +1,11 @@
+select 
+  customers.id as customer_id
+  , name
+  , email
+  , min(created_at) as first_order_at
+  , count(*) as number_of_orders
+from `analytics-engineers-club.coffee_shop.customers` customers
+left join `analytics-engineers-club.coffee_shop.orders` orders on customers.id=orders.customer_id
+group by 1,2,3
+order by 4
+limit 5;
