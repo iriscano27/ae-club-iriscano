6085b113a339657fe8bc1dd965c66c3795486e49
iriscano27 <45745483+iriscano27@users.noreply.github.com>
2023-08-18 18:30:55 +0000

    add sessions model

---
+++ coffee_shop_irisc/models/sessions.sql
@@ -0,0 +1,27 @@
+{{ config(
+    materialized='table'
+ ) }}
+
+with base as (
+select * from {{ ref ('user_stitching') }})
+
+, previous_timestamp as (
+  select *,lag(timestamp) OVER (PARTITION BY visitor_id ORDER BY timestamp) as lagged_timestamp
+  from base)
+
+, diff as (
+  select *, date_diff(timestamp, lagged_timestamp, minute) as difference_minutes
+  from previous_timestamp)
+
+, new_session as (
+  select *, cast(coalesce(difference_minutes > 30, true) as integer) as is_new_session
+  from diff)
+
+, add_session as (
+  select *, sum(is_new_session) over (partition by visitor_id order by timestamp rows between unbounded preceding and current row) as session_number
+  from new_session)
+
+select * 
+  , min(timestamp) over (partition by visitor_id,session_number) as session_start_time 
+  , max(timestamp) over (partition by visitor_id,session_number) as session_end_time 
+from add_session
